5bc6d803e73aee7242739a929307b5e342416e54
Your Name <lylyby1010@gamil.com>
2021-10-30 21:38:15 +0900

    NohDrang2

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://NohDrang2:K4yXjs4RtcPmxtM0@midtermserver.muq8w.mongodb.net/comp229?retryWrites=true&w=majority"
+};
